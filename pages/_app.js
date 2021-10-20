51693e333c0f91c63b8982d472362536ff442400
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-10-20 15:55:21 +0300

    feat: styling with theme-ui, testing it out

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import React from 'react'
+import { ThemeProvider, jsx } from 'theme-ui'
+import theme from '../theme'
+import Nav from '../src/components/nav'
+
+export default function App({ Component, pageProps }) {
+    return (
+      <ThemeProvider theme={theme} >
+        <Nav />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
+/** @jsxRuntime classic */
+/** @jsx jsx */
 
-const Page = () => <div>Index Page</div>
+import { jsx } from 'theme-ui'
+import Link from 'next/link'
 
-export default Page;
+export default () => (
+  <div sx={{ height: `calc(100vh - 60px)`}}>
+    <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
+      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+    </div>
+  </div>
+)
